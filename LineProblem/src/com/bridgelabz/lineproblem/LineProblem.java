0990e3f5ac766fefb49f9e73bd415433cb242743
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-22 22:23:30 +0530

    ADDED: LineProblem file with greetings

---
+++ LineProblem/src/com/bridgelabz/lineproblem/LineProblem.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.lineproblem;
+
+public class LineProblem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to line comparision computation");
+
+	}
+
+}
