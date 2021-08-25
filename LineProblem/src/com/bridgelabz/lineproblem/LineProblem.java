d25a9427d55a298f00d4ba060db4972594555204
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-25 21:51:31 +0530

    ADDED: code to find equaity of two lines

--- LineProblem/src/com/bridgelabz/lineproblem/LineProblem.java
+++ LineProblem/src/com/bridgelabz/lineproblem/LineProblem.java
@@ -6,19 +6,36 @@ public class LineProblem {
 		double length=Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 		return length;
 	}
+	public static void findEquality(String length1,String length2) {
+		if(length1.equals(length2))
+			System.out.println("equal");
+		else
+			System.out.println("not equal");
+  	    
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparision computation");
-		int x1,x2,y1,y2;
-		double length;
+		int x1,x2,x3,x4,y1,y2,y3,y4;
+		double l1,l2;
+		String length1,length2;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter (x1,y1) and (x2,y2) points");
-        x1=sc.nextInt();
-	    y1=sc.nextInt();
+		x1=sc.nextInt();
+		y1=sc.nextInt();
  		x2=sc.nextInt();
  		y2=sc.nextInt();
-        length = findLength(x1, y1, x2, y2);
-		System.out.println("the length is: "+length);
+		System.out.println("enter (x3,y3) and (x4,y4) points");
+        x3=sc.nextInt();
+        y3=sc.nextInt();
+        x4=sc.nextInt();
+        y4=sc.nextInt();
+
+	  	l1=findLength(x1, y1, x2, y2);
+	  	l2=findLength(x3, y3, x4, y4);
+		length1=String.valueOf(Math.round(l1));
+		length2=String.valueOf(Math.round(l2));
+		findEquality(length1, length2);
 		sc.close();
 	}
 
