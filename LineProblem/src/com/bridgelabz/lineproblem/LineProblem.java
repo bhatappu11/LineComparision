1f35fbb28127dab65cc76496ebf1f8ff9ea3fb03
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-25 21:33:42 +0530

    MODIFIED: added method find length

--- LineProblem/src/com/bridgelabz/lineproblem/LineProblem.java
+++ LineProblem/src/com/bridgelabz/lineproblem/LineProblem.java
@@ -2,33 +2,24 @@ package com.bridgelabz.lineproblem;
 
 import java.util.*;
 public class LineProblem {
+	public static double findLength(int x1,int y1, int x2, int y2) {
+		double length=Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+		return length;
+	}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to line comparision computation");
-		int x1,x2,x3,x4,y1,y2,y3,y4;
-		double l1,l2;
-		String length1,length2;
+		int x1,x2,y1,y2;
+		double length;
 		Scanner sc=new Scanner(System.in);
 		System.out.println("enter (x1,y1) and (x2,y2) points");
-                x1=sc.nextInt();
-	        y1=sc.nextInt();
+        x1=sc.nextInt();
+	    y1=sc.nextInt();
  		x2=sc.nextInt();
  		y2=sc.nextInt();
-		System.out.println("enter (x3,y3) and (x4,y4) points");
-                x3=sc.nextInt();
-                y3=sc.nextInt();
-                x4=sc.nextInt();
-                y4=sc.nextInt();
-
-	  	l1=Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
-                l2=Math.sqrt(Math.pow(x4 - x3, 2) + Math.pow(y4 - y3, 2));
-		length1=String.valueOf(Math.round(l1));
-		length2=String.valueOf(Math.round(l2));
-  	        if(length1.equals(length2))
-			System.out.println("equal");
-		else
-			System.out.println("not equal");
-
+        length = findLength(x1, y1, x2, y2);
+		System.out.println("the length is: "+length);
+		sc.close();
 	}
 
 }
