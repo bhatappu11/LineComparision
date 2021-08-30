a1a84140aa61ce34b284c0d6ba7d51593c6e5e6b
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-30 22:21:57 +0530

    REFACTOR: Implemented interface and association concept using oops for point and line

--- LineProblem/src/com/bridgelabz/lineproblem/LineProblem.java
+++ LineProblem/src/com/bridgelabz/lineproblem/LineProblem.java
@@ -1,38 +1,31 @@
 package com.bridgelabz.lineproblem;
 
 import java.util.*;
-
 public class LineProblem {
 	public static void main(String[] args) {
+		Scanner scanner=new Scanner(System.in);
 		System.out.println("Welcome to line comparision computation");
 		LineProblemIF lineComparision = new LineProblemImpl();
-		Scanner sc=new Scanner(System.in);
-		Point startPoint = new Point();
-		Point endPoint = new Point();
-		
-		System.out.println("Enter x start coordinate of line 1");
-		startPoint.setxCoordinate(sc.nextInt());
-		System.out.println("Enter y start coordinate of line 1");
-		startPoint.setyCoordinate(sc.nextInt());
-		System.out.println("Enter x end coordinate of line 1");
-		endPoint.setxCoordinate(sc.nextInt());
-		System.out.println("Enter y end coordinate of line 1");
-		endPoint.setyCoordinate(sc.nextInt());
-		Double lengthOfLine1= lineComparision.findLength(startPoint, endPoint);
-		
-		System.out.println("Enter x start coordinate of line 2");
-		startPoint.setxCoordinate(sc.nextInt());
-		System.out.println("Enter y start coordinate of line 2");
-		startPoint.setyCoordinate(sc.nextInt());
-		System.out.println("Enter x end coordinate of line 2");
-		endPoint.setxCoordinate(sc.nextInt());
-		System.out.println("Enter y end coordinate of line 2");
-		endPoint.setyCoordinate(sc.nextInt());
-		Double lengthOfLine2= lineComparision.findLength(startPoint, endPoint);
-		
-		lineComparision.findEquality(lengthOfLine1, lengthOfLine2);
-		lineComparision.compareLines(lengthOfLine1, lengthOfLine2);
-
+		Line line1 = new Line();
+		Line line2 = new Line();
+		System.out.println("Enter Line 1 coordinates - (x1,y1) and (x2,y2)");
+		int line1X1 = scanner.nextInt();
+		int line1Y1 = scanner.nextInt();
+		line1.setStartCoordinates(line1X1, line1Y1);
+		int line1X2 = scanner.nextInt();
+		int line1Y2 = scanner.nextInt();
+		line1.setEndCoordinates(line1X2, line1Y2);
+		System.out.println("Enter Line 2 Coordinates - (x1,y1) and (x2,y2)");
+		int line2X1 = scanner.nextInt();
+		int line2Y1 = scanner.nextInt();
+		line2.setStartCoordinates(line2X1, line2Y1);
+		int line2X2 = scanner.nextInt();
+		int line2Y2 = scanner.nextInt();
+		line2.setEndCoordinates(line2X2, line2Y2);
+					
+		lineComparision.findEquality(line1.findLength(),line2.findLength());
+		lineComparision.compareLines(line1.findLength(),line2.findLength());
+		scanner.close();
 	}
 
 }
--- LineProblem/src/com/bridgelabz/lineproblem/LineProblemImpl.java
+++ LineProblem/src/com/bridgelabz/lineproblem/LineProblemImpl.java
@@ -1,13 +1,6 @@
 package com.bridgelabz.lineproblem;
 
 public class LineProblemImpl implements LineProblemIF {
-
-	@Override
-	public Double findLength(Point startPoint, Point endPoint) {
-		Double length = Double.valueOf(Math.sqrt(Math.pow((startPoint.xCoordinate-endPoint.xCoordinate),2)+Math.pow((startPoint.yCoordinate-endPoint.yCoordinate),2)));
-		return length;
-	}
-
 	@Override
 	public void findEquality(Double lengthOfLine1, Double lengthOfLine2) {
 		if(lengthOfLine1.equals(lengthOfLine2)) {
